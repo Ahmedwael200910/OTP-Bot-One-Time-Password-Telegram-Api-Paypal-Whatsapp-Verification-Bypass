ac90a0d90cc6610abec84dd0e5ba02d1e85405e9
Ahmedwael200910 <150148979+Ahmedwael200910@users.noreply.github.com>
2024-01-16 00:02:18 +0200

    Update __init__.py

--- OTP Bot Telegram/TelegramBot/__init__.py
+++ OTP Bot Telegram/TelegramBot/__init__.py
@@ -1,4 +1,4 @@
 from telethon import TelegramClient
 from config import vars
 
-bot = TelegramClient('bot_session', vars.api_id, vars.api_hash).start(bot_token=vars.bot_token) 
+bot = TelegramClient('bot_session', vars.api_id, vars.api_hash).start(6597282081:AAFHRb_6vLfpb0PA--HHWbP3GMptZ14IZNk=vars.6597282081:AAFHRb_6vLfpb0PA--HHWbP3GMptZ14IZNk) 
